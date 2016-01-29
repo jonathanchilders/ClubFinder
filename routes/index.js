5653ef16fc31f2b163b724782db99378f541d3bd
Jonathan McAlexander <jmmcalex@gmail.com>
2016-01-29 01:47:24 +0000

    Created a homepage

--- routes/index.js
+++ routes/index.js
@@ -9,38 +9,21 @@ exports.view = function(req, res){
 
 exports.view = function(req, res){
   res.render('index', {
-    'projects': [
-      { 'name': 'Waiting in Line',
-        'image': 'lorempixel.people.1.jpeg',
-        'id': 'project1'
+    'homepage_options': [
+      { 'name': 'Create a Club Profile',
+        'id': 'create_profile_button'
       },
-      { 'name': 'Needfinding',
-        'image': 'lorempixel.city.1.jpeg',
-        'id': 'project2'
+      { 'name': 'Find Clubs for You',
+        'id': 'find_club_button'
       },
-      { 'name': 'Prototyping',
-        'image': 'lorempixel.technics.1.jpeg',
-        'id': 'project3'
+      { 'name': 'Browse Clubs',
+        'id': 'browse_clubs_button'
       },
-      { 'name': 'Heuristic Evaluation',
-        'image': 'lorempixel.abstract.1.jpeg',
-        'id': 'project4'
+      { 'name': 'Calendar of Events',
+        'id': 'calendar_button'
       },
-      { 'name': 'Visualization',
-        'image': 'lorempixel.abstract.8.jpeg',
-        'id': 'project5'
-      },
-      { 'name': 'Social design',
-        'image': 'lorempixel.people.2.jpeg',
-        'id': 'project6'
-      },
-      { 'name': 'Gestural interaction',
-        'image': 'lorempixel.technics.2.jpeg',
-        'id': 'project7'
-      },
-      { 'name': 'Design tools',
-        'image': 'lorempixel.city.2.jpeg',
-        'id': 'project8'
+      { 'name': 'Register Your Club',
+        'id': 'register_club_button'
       }
     ]
   });
