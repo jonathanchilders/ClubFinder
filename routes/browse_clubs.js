db74e2e1e2593602f3cf019fb1deb1f1d25e085a
Jonathan Childers <jonathanchilders65@gmail.com>
2016-02-10 18:41:20 -0800

    set up browse clubs page to sort clubs by name

--- routes/browse_clubs.js
+++ routes/browse_clubs.js
@@ -2,5 +2,18 @@ var data = require('../data.json');
 
 exports.view = function(req, res){
 	console.log(data);
+	data['Clubs'].sort(sort_by('name', false, function(a){return a.toUpperCase()}));
 	res.render('browse_clubs', data);
 };
+
+var sort_by = function(field, reverse, primer){
+   var key = primer ? 
+       function(x) {return primer(x[field])} : 
+       function(x) {return x[field]};
+
+   reverse = !reverse ? 1 : -1;
+
+   return function (a, b) {
+       return a = key(a), b = key(b), reverse * ((a > b) - (b > a));
+   } 
+}
