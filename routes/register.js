0e398c2c98284ba941a522c280a5ed500c4b93bf
Jonathan Childers <jonathanchilders65@gmail.com>
2016-02-11 19:51:02 -0800

    fixed button in create profile

--- routes/register.js
+++ routes/register.js
@@ -1,6 +1,5 @@
 var tags = require('../tags.json');
-var clubs = require('../clubs.json');
 
 exports.view = function(req, res){
-  res.render('register', {tags, clubs});
+  res.render('register', tags);
 };
