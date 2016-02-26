4b55ca4dc861be0351db9f2c1308a4f80a195c2a
Jonathan Childers <jonathanchilders65@gmail.com>
2016-02-25 16:04:53 -0800

    Added more validation for profile creation

--- public/js/profile_validation.js
+++ public/js/profile_validation.js
@@ -16,22 +16,73 @@ function submitClick(e)
 	var password = $("#password").val();
 	var confirmPassword = $("#confirm_password").val();
 
-	if(password === confirmPassword)
+	$.get('/users', function(data){
+			validateCredentials(data, name, email, password, confirmPassword);
+		});
+}
+
+function validateCredentials(data, name, email, password, confirmPassword)
+{
+	if(name != "" && email != "" && password != "")
 	{
-		var user = 
+		if(email.indexOf("@") != -1)
 		{
-			"name": name,
-			"email": email,
-			"password": password
+			if(emailUnused(data["Users"], email))
+			{
+				if(password === confirmPassword)
+				{
+					var user = 
+					{
+						"name": name,
+						"email": email,
+						"password": password
+					}
+					makePostRequest(JSON.stringify(user));
+					window.location.href = "/login";
+				}
+				else
+				{
+					$("#alert_box").html("<h2>Password And Confirm Password Do Not Match</h2>");
+				}
+			}
+			else
+			{
+				$("#alert_box").html("<h2>Email is already in use</h2>");
+			}
 		}
-		makePostRequest(JSON.stringify(user));
-		window.location.href = "/login";
+		else
+		{
+			$("#alert_box").html("<h2>Email is not valid</h2>");
+		}
 	}
 	else
 	{
-		$("#alert_box").html("<h2>Password and Confirm Password do not match</h2>");
+		if(password === "")
+		{
+			$("#alert_box").html("<h2>Password cannot be empty<h2>");		
+		}
+		if(email === "")
+		{
+			$("#alert_box").html("<h2>Email cannot be empty<h2>");
+		}	
+		if(name === "")
+		{
+			$("#alert_box").html("<h2>Username cannot be empty<h2>");
+		}
 	}
-	
+}
+
+function emailUnused(users, email)
+{
+	for(var i = 0; i < users.length; i++)
+	{	
+		var user = users[i];
+		if(user["email"] === email)
+		{
+			return false;
+		}
+	}
+	return true;
 }
 
 function makePostRequest(userData)
@@ -48,3 +99,6 @@ function makePostRequest(userData)
 	        	}
 			});
 }
+
+
+
